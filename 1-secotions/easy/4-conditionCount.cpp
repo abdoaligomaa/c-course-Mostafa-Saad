14023cf78c0411e1ddf9a5805bfa205980d2a770
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-04-20 09:30:31 +0200

    the fourth problem in easy and finish the easy

--- 1-secotions/easy/4-conditionCount.cpp
+++ 1-secotions/easy/4-conditionCount.cpp
@@ -1,37 +1,44 @@
 // read one integers
 // then read five integers
 // print how many numbers in less than or equal x and
-// print how many numbers in greater than x 
+// print how many numbers in greater than x
 
 #include <iostream>
 using namespace std;
 int main()
 {
-    int x, num1, num2, num3, num4, num5, coutn_max, count_min;
+    int x, num1, num2, num3, num4, num5;
+    int coutn_max, count_min;
     cout << "Enter the integer :";
     cin >> x;
     cout << "Enter the five integers :";
     cin >> num1 >> num2 >> num3 >> num4 >> num5;
-    if (num1 < x)
-    {
-        count_min++;
-    }
-    if (num2 < x)
-    {
-        count_min++;
-    }
-    if (num3 < x)
-    {
-        count_min++;
-    }
-    if (num4 < x)
-    {
-        count_min++;
-    }
-    if (num5 < x)
-    {
-        count_min++;
-    }
+    count_min += (num1 <= x);
+    count_min += (num2 <= x);
+    count_min += (num3 <= x);
+    count_min += (num4 <= x);
+    count_min += (num5 <= x);
+    // if (num1 < x)
+    // {
+    //     count_min++;
+    // }
+    // if (num2 < x)
+    // {
+    //     count_min++;
+    // }
+    // if (num3 < x)
+    // {
+    //     count_min++;
+    // }
+    // if (num4 < x)
+    // {
+    //     count_min++;
+    // }
+    // if (num5 < x)
+    // {
+    //     count_min++;
+    // }
+
     coutn_max = 5 - count_min;
 
     cout << count_min << " " << coutn_max;
