8470b93a281e8b0b5207b162574f7b8c424fa64c
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-04-23 18:24:22 +0200

    solve the first proble in easy way while loop

---
+++ 2-loop/easy/1-easyRange.cpp
@@ -0,0 +1,19 @@
+// read two integers x and y
+// print the numbers between x and y
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout <<"Enter the two integers x and y :";
+    cin>>x>>y;
+
+   while(x<=y){
+       cout<<x<<" ";
+       x++;
+   }
+
+    return 0;
+}
+
+// end of the programm 
